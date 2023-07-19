1a76f633191b8b18f4807991c0d7121dd6ffbec9
freshhuk <freshhuk13@gmail.com>
2023-07-19 22:48:21 +0300

    i started create xunit tests

---
+++ UnitTests/TaskControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using ToDoListWeb.Controllers;
+using Microsoft.Extensions.Logging;
+using ToDoListWeb.Entity;
+
+namespace UnitTests
+{
+    public class TaskControllerTests
+    {
+        [Fact]
+        public void DeleteTaskTest()
+        {
+            // Arrange
+            /*
+            var dbContextMock = new Mock<TaskDbContex>();
+            var loggerMock = new Mock<ILogger<TaskController>>();
+
+            var controller = new TaskController(dbContextMock.Object, loggerMock.Object);
+            */
+        }
+    }
+}
