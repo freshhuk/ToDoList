10371d1f58f9867cb9c25d88b6dfa0366d906dc2
freshhuk <freshhuk13@gmail.com>
2023-07-20 11:33:51 +0300

    i started create xunit tests for TaskController

--- UnitTests/TaskControllerTests.cs
+++ UnitTests/TaskControllerTests.cs
@@ -3,24 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xunit;
+using Moq;
 using ToDoListWeb.Controllers;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using ToDoListWeb.Entity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UnitTests
 {
     public class TaskControllerTests
     {
         [Fact]
-        public void DeleteTaskTest()
+        public async Task DeleteTaskTest()
         {
-            // Arrange
-            /*
-            var dbContextMock = new Mock<TaskDbContex>();
-            var loggerMock = new Mock<ILogger<TaskController>>();
+            // Arrange создаем скажем так обьект нашего класа
+
+            var configurationMock = new Mock<IConfiguration>();
+            var dbContextMock = new Mock<TaskDbContex>(configurationMock.Object); var loggerMock = new Mock<ILogger<TaskController>>();
 
             var controller = new TaskController(dbContextMock.Object, loggerMock.Object);
-            */
+
+            //Act выполняет метод
+
+            var result = await controller.DeleteTaskDBb(3) as RedirectResult;
+
+            //Assert верифицирует результат теста, тоесть мы ставим некие условия для нашего теста 
+
+            Assert.NotNull(result);
+            
+            
         }
     }
 }
