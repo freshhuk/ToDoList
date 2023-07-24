df645283441b2f4f462486e2d502c469f1ba6014
freshhuk <freshhuk13@gmail.com>
2023-07-24 23:07:39 +0300

    finish first test and fix bugs

--- UnitTests/TaskControllerTests.cs
+++ UnitTests/TaskControllerTests.cs
@@ -8,31 +8,90 @@ using ToDoListWeb.Controllers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using ToDoListWeb.Entity;
+using ToDoListWeb.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace UnitTests
 {
+    class TestDbTasks : IDataContext
+    {
+        public DbSet<ToDoTask> ToDoTask { get; set; }
+        public List<ToDoTask> TestData = new List<ToDoTask>()
+        {
+           new ToDoTask() { Id = 0, },
+           new ToDoTask() { Id = 1, },
+           new ToDoTask() { Id = 2, },
+           new ToDoTask() { Id = 3, },
+        };
+
+        public async Task AddAsync<TEntity>(TEntity entity) where TEntity : class
+        {
+            TestData.Add(entity as ToDoTask);
+            await Task.CompletedTask; // Имитируем асинхронное выполнение
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await Task.CompletedTask; // Имитируем асинхронное выполнение
+        }
+
+        // Метод для поиска задачи по Id
+        public async Task<ToDoTask> FindAsync(int Id)
+        {
+            return await Task.FromResult(TestData.FirstOrDefault(task => task.Id == Id));
+        }
+
+        // Метод для удаления задачи по Id
+        public async Task RemoveAsync(int Id)
+        {
+            var taskToRemove = await FindAsync(Id);
+            if (taskToRemove != null)
+            {
+                TestData.Remove(taskToRemove);
+            }
+        }
+        public void ClearTestData()
+        {
+            TestData.Clear();
+        }
+
+        // Метод для получения всех тестовых задач из имитации базы данных
+        public List<ToDoTask> GetToDoTasks()
+        {
+            return TestData;
+        }
+    }
     public class TaskControllerTests
     {
         [Fact]
-        public async Task DeleteTaskTest()
+        public async Task GetTaskDb()
         {
             // Arrange создаем скажем так обьект нашего класа
 
-            var configurationMock = new Mock<IConfiguration>();
-            var dbContextMock = new Mock<TaskDbContex>(configurationMock.Object); var loggerMock = new Mock<ILogger<TaskController>>();
-
-            var controller = new TaskController(dbContextMock.Object, loggerMock.Object);
+            var dbcontextMock = new TestDbTasks();
+            var loggerMock = new Mock<ILogger<TaskController>>();
+            dbcontextMock.ClearTestData();
+            var controller = new TaskController(dbcontextMock, loggerMock.Object);
 
             //Act выполняет метод
 
-            var result = await controller.DeleteTaskDBb(3) as RedirectResult;
+            var result = await controller.GetTaskDb("Test", "Description", DateTime.Now);
 
             //Assert верифицирует результат теста, тоесть мы ставим некие условия для нашего теста 
 
-            Assert.NotNull(result);
-            
-            
+            // Проверяем, что в базе данных была добавлена задача
+            var tasksInDb = dbcontextMock.GetToDoTasks();
+            Assert.Single(tasksInDb); // Проверяем, что в базе данных только одна задача
+
+            // Добавьте дополнительные утверждения для проверки значений задачи, если необходимо
+            var addedTask = tasksInDb.First();
+            Assert.Equal("Test", addedTask.NameTask);
+            Assert.Equal("Description", addedTask.DescriptionTask);
+            Assert.Equal(DateTime.Now.Date, addedTask.TaskTime.Date);
+            Assert.Equal("In progress", addedTask.Status);
+
+
         }
     }
 }
