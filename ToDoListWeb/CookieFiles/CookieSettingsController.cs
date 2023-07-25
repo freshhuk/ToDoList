398197ee86201d1dd7f86a1cc86de87f2c716a1a
freshhuk <freshhuk13@gmail.com>
2023-07-25 12:27:18 +0300

    i created cookie method for theme

---
+++ ToDoListWeb/CookieFiles/CookieSettingsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+
+namespace ToDoListWeb.CookieFiles
+{
+    public class CookieSettingsController : Controller
+    {
+        public IActionResult CookieThemeSettings(string ThemeSettings)
+        {
+            if (!string.IsNullOrEmpty(ThemeSettings))
+            {
+                Response.Cookies.Append("SettingsTheme", ThemeSettings, new CookieOptions
+                {
+                    // Настройки куки (например, время жизни и доступность)
+                    Expires = DateTime.Now.AddDays(7) // Здесь можно установить желаемое время жизни куки
+                });
+            } 
+            return View();
+        }
+
+    }
+}
