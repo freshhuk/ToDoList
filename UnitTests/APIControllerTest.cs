79bae81d343050efe3ef6800adb79b326681a6e3
freshhuk <freshhuk13@gmail.com>
2023-10-03 23:22:31 +0300

    I  created mock object

--- UnitTests/APIControllerTest.cs
+++ UnitTests/APIControllerTest.cs
@@ -1,10 +1,23 @@
+using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using Moq;
+using ToDoListWebAPI.Controllers;
+using UnitTests.MockEntities;
+
 namespace UnitTests
 {
     public class APIControllerTest
     {
         [Fact]
-        public void Test1()
+        public void AddTaskDbTest()
         {
+            //Arrange
+            var mockILogger = new Mock<ILogger<TaskController>>();
+            var mockDbContext = new MockToDoTask();
+
+            //var controller = new TaskController(mockILogger.Object, mockDbContext.);
+
+
 
         }
     }
---
+++ UnitTests/MockEntities/MockTestDbContext.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoListWebDomain.Domain.Entity;
+using ToDoListWebInfrastructure.Interfaces;
+
+namespace UnitTests.MockEntities
+{
+    internal class MockTestDbContext : IDataContext<MockToDoTask>
+    {
+        private List<MockToDoTask> dataStore = new List<MockToDoTask>();
+
+        public async Task AddAsync(MockToDoTask item)
+        {
+            dataStore.Add(item);
+        }
+
+        public void Delete(int id)
+        {
+            var item = dataStore.SingleOrDefault(entity => GetId(entity) == id);
+            if (item != null)
+            {
+                dataStore.Remove(item);
+            }
+        }
+
+        public MockToDoTask Get(int id)
+        {
+            var item = dataStore.SingleOrDefault(entity => GetId(entity) == id);
+            if (item == null)
+            {
+                throw new InvalidOperationException("Item not found.");
+            }
+            return item;
+        }
+
+        public IEnumerable<MockToDoTask> GetAll()
+        {
+            return dataStore;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            // В данном случае, для мока базы данных, сохранение изменений может быть пустым методом,
+            // так как нет реальной базы данных.
+            await Task.CompletedTask;
+        }
+
+        public async Task Update(MockToDoTask item)
+        {
+            var id = GetId(item);
+            var existingItem = dataStore.SingleOrDefault(entity => GetId(entity) == id);
+            if (existingItem != null)
+            {
+                // Обновляем поля существующего элемента
+                UpdateItem(existingItem, item);
+            }
+        }
+
+        // Метод для получения уникального идентификатора элемента
+        private int GetId(MockToDoTask item)
+        {
+            return item.Id;
+        }
+
+        // Метод для обновления полей существующего элемента
+        private void UpdateItem(MockToDoTask existingItem, MockToDoTask newItem)
+        {
+
+            existingItem.Id = newItem.Id;
+            existingItem.NameTask = newItem.NameTask;
+            existingItem.DescriptionTask = newItem.DescriptionTask;
+            existingItem.Status = newItem.Status;
+            existingItem.TaskTime = newItem.TaskTime;
+        }
+    }
+}
